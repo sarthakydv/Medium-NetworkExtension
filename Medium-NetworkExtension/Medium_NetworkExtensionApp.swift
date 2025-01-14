1630d5371394c23b17bb2adeb1f6d9ee8d371cdd
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2025-01-14 21:38:19 +0700

    Initial Commit

---
+++ Medium-NetworkExtension/Medium_NetworkExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medium_NetworkExtensionApp.swift
+//  Medium-NetworkExtension
+//
+//  Created by Dason Tiovino on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Medium_NetworkExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Medium-NetworkExtensionTests/Medium_NetworkExtensionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Medium_NetworkExtensionTests.swift
+//  Medium-NetworkExtensionTests
+//
+//  Created by Dason Tiovino on 14/01/25.
+//
+
+import Testing
+@testable import Medium_NetworkExtension
+
+struct Medium_NetworkExtensionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
